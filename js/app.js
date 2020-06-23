fab37701b44bc3c09a8222fb2cbdb5f9c39c5937
Pascal <it.tech@laposte.net>
2020-06-23 11:32:47 +0200

    design page formation

--- js/app.js
+++ js/app.js
@@ -84,7 +84,14 @@ $(document).ready(function () {
     return false;
   });
 
-
+  $('.btnContactDirect').on('click', function () { // Au clic sur un élément
+    var page = $(this).attr('href'); // Page cible
+    var speed = 750; // Durée de l'animation (en ms)
+    $('html, body').animate({
+      scrollTop: $(page).offset().top
+    }, speed); // Go
+    return false;
+  });
 
 
 });
